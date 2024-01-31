84edd6b3dbb502d2f021d556022e62eb6b6868bd
devb15 <34093122+devb15@users.noreply.github.com>
2024-01-31 09:17:23 +0000

    Added Comments

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var ServiceController = require('./../../Controllers/ServiceController');
 var passport = require('passport');
 
-
+//Controller for Service
 router.post('/service',ServiceController.postService);
 
 
